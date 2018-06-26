23d0933d61385e68830cb3c3be12a163468750d1
events <events@live.cn>
2018-06-26 21:38:33 +0800

    test

---
+++ src/main/java/com/thinkgem/jeesite/test/web/TestDate.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.thinkgem.jeesite.test.web;
+
+/**
+ * @author WZ
+ *
+ */
+public class TestDate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
